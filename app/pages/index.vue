dbdc80a969315e13a5cb94b361cf196b24edb8ff
edu souza <eduardosouza12pw@gmail.com>
2025-08-19 16:26:09 -0400

    pages

---
+++ app/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-zinc-800 text-white min-h-screen flex flex-col">
+    <div class="flex-1">
+      <div class="max-w-6xl mx-auto px-4 py-6 flex flex-col gap-8">
+        <HeaderAppHeader/>
+        <HeroSection/>
+      </div>
+      <div class="w-full bg-zinc-700 py-5 rounded-xl">
+        <div class="max-w-6xl mx-auto w-full flex flex-col md:flex-row md:gap-20 p-5 justify-center items-center">
+          <AboutSection/>
+          <BoxSkills/>
+        </div>
+      </div>
+    </div>
+    <AppFooter/>
+  </div>
+</template>
+
+<script lang="ts">
+import { AppFooter } from '#components';
+
+
+</script>
+
+<style scoped>
+
+</style>
