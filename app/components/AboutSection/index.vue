e26f85005b6eac0837bd30d3d8362b16fa91eadf
edu souza <eduardosouza12pw@gmail.com>
2025-08-19 16:24:56 -0400

    add sobre mim

---
+++ app/components/AboutSection/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full flex flex-col items-center">
+    <section class="bg-zinc-700 rounded-xl p-6 w-full">
+      <h2 class="text-xl md:text-2xl font-bold mb-3 text-center">Sobre mim</h2>
+      <p class="text-base md:text-lg text-gray-200 text-center leading-relaxed">
+        Utilizo para o desenvolvimento front-end <span class="font-semibold text-blue-300">JavaScript</span>, <span class="font-semibold text-blue-300">TypeScript</span> e <span class="font-semibold text-blue-300">Vue</span>. Bla bla bla.
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
