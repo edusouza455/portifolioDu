5c57aab04bd0ed456652eaa3c1b4d00f25de19da
edu souza <eduardosouza12pw@gmail.com>
2025-08-19 16:25:03 -0400

    add rodape

---
+++ app/components/AppFooter/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="w-full text-center py-4 text-gray-400 text-sm">
+  Desenvolvido por Eduardo Souza
+</footer>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
