fd250d061efd335c0b0e14700d24a696322d6a48
Edu Souza <167351223+edusouza455@users.noreply.github.com>
2025-08-20 16:29:23 -0400

    Update index.vue

--- app/components/AboutSection/index.vue
+++ app/components/AboutSection/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full flex flex-col items-center">
     <section class="bg-zinc-700 rounded-xl p-6 w-full">
-      <h2 class="text-xl md:text-2xl font-bold mb-3 text-center">Sobre mim</h2>
+      <h2 class="text-xl md:text-2xl font-bold mb-3 text-center">Sobre </h2>
   <p class="text-base md:text-lg text-gray-200 text-justify leading-relaxed">
     Possui mais de 2 anos de experiência em desenvolvimento web, atuando em projetos de front-end e back-end.<br>
     No marketplace, trabalhou com <span class="font-semibold text-blue-300">Django</span> no back-end e <span class="font-semibold text-blue-300">Tailwind CSS</span> no front-end, garantindo qualidade e usabilidade.<br>
