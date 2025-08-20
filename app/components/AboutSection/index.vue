5ae7bbea7116f241d86c2dd41623d80a885d2aa3
edu souza <edu.testes456@gmail.com>
2025-08-20 16:14:25 -0400

    novas imagens

--- app/components/AboutSection/index.vue
+++ app/components/AboutSection/index.vue
@@ -2,9 +2,13 @@
   <div class="w-full flex flex-col items-center">
     <section class="bg-zinc-700 rounded-xl p-6 w-full">
       <h2 class="text-xl md:text-2xl font-bold mb-3 text-center">Sobre mim</h2>
-      <p class="text-base md:text-lg text-gray-200 text-center leading-relaxed">
-        Utilizo para o desenvolvimento front-end <span class="font-semibold text-blue-300">JavaScript</span>, <span class="font-semibold text-blue-300">TypeScript</span> e <span class="font-semibold text-blue-300">Vue</span>. Bla bla bla.
-      </p>
+  <p class="text-base md:text-lg text-gray-200 text-justify leading-relaxed">
+    Possui mais de 2 anos de experiência em desenvolvimento web, atuando em projetos de front-end e back-end.<br>
+    No marketplace, trabalhou com <span class="font-semibold text-blue-300">Django</span> no back-end e <span class="font-semibold text-blue-300">Tailwind CSS</span> no front-end, garantindo qualidade e usabilidade.<br>
+    Na plataforma de concursos e seletivos, participou desde a fase inicial, contribuindo para a arquitetura e padronização com <span class="font-semibold text-blue-300">Next.js</span>, <span class="font-semibold text-blue-300">Prisma</span> e <span class="font-semibold text-blue-300">TypeScript</span>.<br>
+    Em todos os projetos utilizou <span class="font-semibold text-blue-300">Docker</span> e atuou em colaboração com a equipe de desenvolvimento, assegurando consistência e escalabilidade.
+</p>
+
     </section>
   </div>
 </template>
